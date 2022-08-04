a1b2696700a2a9c240e1f27fdf3a4878525fada4
Rafael Ponte <rafael.ponte@zup.com.br>
2022-08-04 16:06:16 -0300

    test for Repeatable Read Isolation Level;

---
+++ src/test/java/br/com/zup/edu/raceconditions/services/RepeatableReadIsolationLevelNewTicketServiceTest.java
@@ -0,0 +1,52 @@
+package br.com.zup.edu.raceconditions.services;
+
+import base.SpringBootIntegrationTest;
+import br.com.zup.edu.raceconditions.model.Event;
+import br.com.zup.edu.raceconditions.model.EventRepository;
+import br.com.zup.edu.raceconditions.model.TicketRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RepeatableReadIsolationLevelNewTicketServiceTest extends SpringBootIntegrationTest {
+
+	@Autowired
+	private RepeatableReadIsolationLevelNewTicketService repeatableReadIsolationLevelNewTicketService;
+
+	@Autowired
+	private EventRepository eventRepository;
+	@Autowired
+	private TicketRepository ticketRepository;
+
+	private Event EVENT;
+
+	@BeforeEach
+	public void setUp() {
+		ticketRepository.deleteAll();
+		eventRepository.deleteAll();
+		this.EVENT = eventRepository.save(new Event("ZupCon", 5));
+	}
+
+	/**
+     * xxx
+	 */
+	@Test
+	void shouldBuyNoMoreThanMaxTickets() throws InterruptedException {
+
+		assertEquals(5,
+				eventRepository.getMaxTickets(EVENT.getId()));
+
+		doSyncAndConcurrently(10, customerName -> {
+			repeatableReadIsolationLevelNewTicketService.buyNewTicket(EVENT.getId(), customerName);
+		});
+
+		Long ticketCount = ticketRepository.countByEvent(EVENT);
+		LOGGER.info("[?] ticketCount = {}", ticketCount);
+		assertThat(ticketCount)
+				.isLessThanOrEqualTo(5);
+	}
+
+}
