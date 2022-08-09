92bd2c1c042e0370b974824a872427df8e75cd5b
Rafael Ponte <rafael.ponte@zup.com.br>
2022-08-09 15:05:08 -0300

    test for Pessimistic Locking;

--- src/main/java/br/com/zup/edu/raceconditions/account/AccountRepository.java
+++ src/main/java/br/com/zup/edu/raceconditions/account/AccountRepository.java
@@ -1,11 +1,15 @@
 package br.com.zup.edu.raceconditions.account;
 
+import br.com.zup.edu.raceconditions.tickets.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
@@ -17,5 +21,10 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
             where c.id = :accountId
            """)
     public BigDecimal getBalance(Long accountId);
+
+    @Transactional
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select c from Account c where c.id = :accountId")
+    public Optional<Account> findByIdWithPessimisticLocking(Long accountId);
 
 }
