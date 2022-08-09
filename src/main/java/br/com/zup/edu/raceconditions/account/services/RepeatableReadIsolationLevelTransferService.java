aa11596a1c1ce5175a7c650d14f8b5c03f827768
Rafael Ponte <rafael.ponte@zup.com.br>
2022-08-09 18:20:58 -0300

    test for Repeatable Read Isolation Level;

---
+++ src/main/java/br/com/zup/edu/raceconditions/account/services/RepeatableReadIsolationLevelTransferService.java
@@ -0,0 +1,39 @@
+package br.com.zup.edu.raceconditions.account.services;
+
+import br.com.zup.edu.raceconditions.account.model.Account;
+import br.com.zup.edu.raceconditions.account.model.AccountRepository;
+import br.com.zup.edu.raceconditions.account.model.Transfer;
+import br.com.zup.edu.raceconditions.account.model.TransferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+@Service
+public class RepeatableReadIsolationLevelTransferService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private TransferRepository transferRepository;
+
+    @Transactional(
+        isolation = Isolation.REPEATABLE_READ
+    )
+    public void transfer(Long fromAccountId, Long toAccountId, BigDecimal amount) {
+
+        Account fromAccount = accountRepository.findById(fromAccountId).orElseThrow(() -> {
+            throw new IllegalStateException("from-account does not exist: " + fromAccountId);
+        });
+
+        Account toAccount = accountRepository.findById(toAccountId).orElseThrow(() -> {
+            throw new IllegalStateException("to-account does not exist: " + toAccountId);
+        });
+
+        Transfer transfer = fromAccount.transferTo(toAccount, amount);
+        transferRepository.save(transfer);
+    }
+
+}
